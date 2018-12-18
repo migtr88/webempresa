82c69921a895a7a4c86bafc48af30b5b46d1f99c
Tabernero Ridao, Miguel <m.tabernero.ridao@accenture.com>
2018-12-18 08:59:35 +0100

    Modificación de permisos de usuarios en admin

--- social/admin.py
+++ social/admin.py
@@ -3,5 +3,16 @@ from .models import Link
 # Register your models here.
 class LinkAdmin(admin.ModelAdmin):
     readonly_fields = ["created", "updated"]
+#Para extender la función de readonly_fields en tiempo de ejecución, es decir q cuando se esta ejecutando la aplicación 
+#se pueda hacer que determinados campos sean solo de lectura, nos interesa hacerlo para ciertos usuarios,declaramos el método
+    def get_readonly_fields(self, request, obj=None):
+        #si el usuario pertenece al grupo de usuarios de Personal 
+        if request.user.groups.filter(name = "Personal"):
+            #devolvemos como readonly_fields los campos del modelo de appsocial q nos interesa dejar de lectura
+            return('created','updated','key','name')
+        #si no fuera así devolvemos los normales 
+        else:
+            return('created','updated')
+
 
 admin.site.register(Link, LinkAdmin)
